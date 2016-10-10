d15e2b68cdb767d6b93603f48ae20594f0235b66
bradymat <bradymat@gmail.com>
2016-10-10 20:09:14 +1300

    deploying

--- components/navbar.js
+++ components/navbar.js
@@ -5,7 +5,7 @@ const Navbar = (props) => {
   return (
     <div>
       <div className='bannerImage .col-md-12' id='home'>
-        <div className='image'><a href='#home'><img src='public/images/me.png'/></a></div>
+        <div className='image'><a href='#home'><img src='https://s.gravatar.com/avatar/b7c8db5fd09d5273b798e8eb17122808?size=496&default=retro'/></a></div>
         <div className='name .col-md-4'><h1>Brady Gillies</h1><h2>Junior Web Developer</h2></div>
       </div>
       <div className='navbar .col-md-4'>
