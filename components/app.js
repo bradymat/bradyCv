a8a9236169388455c03ddb1b3e8a8d298dcd29c5
bradymat <bradymat@gmail.com>
2016-10-10 14:18:42 +1300

    now add some content

--- components/app.js
+++ components/app.js
@@ -5,13 +5,21 @@ class App extends Component {
 
   constructor (props) {
     super(props)
+    this.state = {
+      navButtons: [
+        {id: 0, title: 'Home', href: '#'},
+        {id: 1, title: 'Skills', href: '#'},
+        {id: 2, title: 'Experience', href: '#'},
+        {id: 3, title: 'Contact', href: '#'}
+      ]
+    }
   }
 
   render () {
 
     return (
     <div>
-    <Navbar/>
+    <Navbar navButtons={this.state.navButtons}/>
     <h1>Welcome to {this.props.name}</h1>
     </div>)
   }
--- components/navbar.js
+++ components/navbar.js
@@ -3,16 +3,16 @@ import React from 'react'
 const Navbar = (props) => {
   return (
     <div>
-      <div className='bannerImage'>
-        <div className='image'></div>
-        <div className='name'></div>
-        <div className='nameText'><h1>Brady Gillies</h1><h2>Junior Web Developer</h2></div>
+      <div className='bannerImage .col-md-12'>
+        <div className='image'><img src='public/images/me.png'/></div>
+        <div className='name .col-md-4'><h1>Brady Gillies</h1><h2>Junior Web Developer</h2></div>
       </div>
-      <div className='navbar'>
-        <div className='navButton'><a href='#'>Home</a></div>
-        <div className='navButton'><a href='#'>Skills</a></div>
-        <div className='navButton'><a href='#'>Experience</a></div>
-        <div className='navButton'><a href='#'>Contact</a></div>
+      <div className='navbar .col-md-4'>
+        {
+          props.navButtons.map(function (button) {
+            return <div className='navButton' key={button.id}><a href={button.href}>{button.title}</a></div>
+          })
+        }
       </div>
     </div>)
 }
